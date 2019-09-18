3f836491b25ad7aad87eca91b4b5161bc91c41ad
yymcct <yymcct@hotmail.com>
2019-09-18 14:26:40 +0800

    追息单

---
+++ Xw.Zx.Core/Controllers/InterestController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sieve.Services;
+using Xw.Zx.Core.Models.Model;
+
+namespace Xw.Zx.Core.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InterestController : BaseController
+    {
+        private readonly ILogger<MemberController> _logger;
+        public InterestController(ILogger<MemberController> logger
+            , XwZxContext xwZxContext
+            , IMapper mapper
+            , ISieveProcessor sieveProcessor) : base(xwZxContext, mapper, sieveProcessor)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// 生成追息单, 根据 数据:位置信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public HbzsResult PostInterest()
+        {
+            throw new Exception();
+        }
+
+
+    }
+}
