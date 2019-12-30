f5d960fec7bfa799b1b7fdc8512c0946aae5cf07
yymcct <yymcct@qq.com>
2019-12-30 20:00:02 +0800

    安卓

--- Xw.Zx.Android/app/src/main/java/com/example/sqbandroid/MainActivity.java
+++ Xw.Zx.Android/app/src/main/java/com/example/sqbandroid/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.sqbandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.webkit.WebView;
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("http://139.155.8.217/#/pages/login/login");//加载url
     }
 }
