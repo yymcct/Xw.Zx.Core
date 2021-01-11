50bc4ebe15b4e8e602a10e90bd9aaa5b03b7b877
yymcct <yymcct@qq.com>
2021-01-11 21:29:45 +0800

    重构下载页

--- Xw.Zx.Wap/src/view/app/user/download.vue
+++ Xw.Zx.Wap/src/view/app/user/download.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="wrapper">
+  <div class="wrapper1">
     <a href="http://139.155.8.217:8081/zzz20201012.apk" download="zzz.apk">
-    <img :src="require('@/assets/images/bk2.jpg')" alt="">
-     </a>
+      <img :src="require('@/assets/images/bk2.jpg')" alt="" />
+    </a>
     <share :show="isWeiXin" />
   </div>
 </template>
@@ -37,58 +37,13 @@ export default {
 };
 </script>
 <style lang='scss' scoped>
-.wrapper {
-  height: 100vh;
+.wrapper1 {
+  min-height: 100vh;
+  background-color: rgb(233, 114, 24);
   overflow: hidden;
-  .title {
-    padding-top: 50px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
-    h1 {
-      font-size: 28px;
-      font-weight: bold;
-      line-height: 44px;
-    }
-    p {
-      font-size: 20px;
-
-      line-height: 38px;
-    }
-  }
-  .download {
-    padding-top: 50px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    a {
-      background-color: #fff;
-      padding: 20px;
-      border-radius: 50px;
-      color: #ff5000;
-      font-weight: bold;
-    }
-  }
-  .desc {
-    margin: 50px 20px 20px 20px;
-    background-color: #fff;
-    border-radius: 20px;
-    padding: 20px;
-    font-size: 16px;
-    span {
-      color: red;
-    }
-    ul {
-      padding: 10px;
-      padding-left: 20px;
-
-      li {
-        list-style-type: disc;
-        line-height: 24px;
-      }
+  a {
+    img {
+      width: 100%;
     }
   }
 }
