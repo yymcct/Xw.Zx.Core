78d4a20e0fdbba9b02b9dcb1ac68e6b9c65faf76
ssuunnshan <1246469968@qq.com>
2021-01-21 21:32:20 +0800

    查询 同步 订单信息

--- Xw.Zx.Core/Areas/Manager/WechatSubLedger/WechatSubLedgerController.cs
+++ Xw.Zx.Core/Areas/Manager/WechatSubLedger/WechatSubLedgerController.cs
@@ -336,10 +336,125 @@ namespace Xw.Zx.Core.Areas.Manager
             }
         }
 
+        public HbzsManagerResult<string> QueryThirdOrders([FromQuery] GetThirdQueryOrdersRequestPsDto getThirdQueryOrdersRequestPsDto ) {
+            int page = 1;
+            int pgcount = 0;
+            try {
+                if (getThirdQueryOrdersRequestPsDto == null)
+                    throw new Exception("请传入查询开始和截止时间参数");
+                if (getThirdQueryOrdersRequestPsDto.StartTime == null || getThirdQueryOrdersRequestPsDto.EndTime == null)
+                    throw new Exception("开始和截止时间不能为空");
+                GetThirdQueryOrdersResultDto result = GetThirdOrders((DateTime)getThirdQueryOrdersRequestPsDto.StartTime, (DateTime)getThirdQueryOrdersRequestPsDto.EndTime,page);
+                if (result == null)
+                    throw new Exception("未获取到结果");
+                if(result.page<=0)
+                    throw new Exception("未获取到结果");
+                pgcount = result.page;
+                //保存记录
+                SaveHandle(result.content);
+                while (page <= pgcount)
+                {
+                    page = page + 1;
+                    //查询 并保存记录
+                    GetThirdQueryOrdersResultDto rlt = GetThirdOrders((DateTime)getThirdQueryOrdersRequestPsDto.StartTime, (DateTime)getThirdQueryOrdersRequestPsDto.EndTime, page);
+                    if (rlt == null)
+                        break;
+                    if (rlt.content == null)
+                        break;
+                    SaveHandle(rlt.content);
+                }
+
+                return new HbzsManagerResult<string>(HbzsManagerResultCode.Sucess,"查询处理成功！");
+            }
+            catch(Exception ex)
+            {
+                return new HbzsManagerResult<string>(HbzsManagerResultCode.Remote_Service_Error,ex.Message);
+            }
+        }
+        void SaveHandle(List<GetThirdQueryOrdersDto> ps) {
+            try {
+                if (ps == null)
+                    return;
+                foreach (GetThirdQueryOrdersDto r in ps)
+                {
+                    try
+                    {
+                        if (string.IsNullOrWhiteSpace(r.transid) || string.IsNullOrWhiteSpace(r.ordersn) || string.IsNullOrWhiteSpace(r.paytime))
+                            continue;
+                        WechatOrders wechatOrders = _context.WechatOrders.FirstOrDefault(c => c.Out_Order_No == r.ordersn && c.TransactionID == r.transid);
+                        if (wechatOrders != null)
+                            continue;
+                        WechatOrders neworder = new WechatOrders();
+                        neworder.Out_Order_No = r.ordersn;
+                        neworder.TransactionID = r.transid;
+                        neworder.SubState = WechatOrders.WechatOrderState.待申请;
+                        neworder.TranTime = ConvertStringToDateTime(r.paytime);
+                        neworder.Amount = r.price;
+                        decimal je = Math.Floor(neworder.Amount * (1 - WxPayConfig.Config.Rate) * 100) / 100;
+                        neworder.SubCharge = Math.Floor(je * WxPayConfig.Config.MaxRate * 100) / 100;
+                        neworder.PayState = "SUCCESS";
+                        neworder.PayDescription = "支付成功";
+                        _context.WechatOrders.Add(neworder);
+                        _context.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+            catch (Exception ex) {
+            }
+        }
+
+        GetThirdQueryOrdersResultDto GetThirdOrders(DateTime startTime,DateTime endTime,int curpage) {
+
+            string key = "flu6L9KdZEA9HtdClxp9BcGNjE2QMNdZPXZtLmzho6k";
+            string timestr = Convert.ToInt64((DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds).ToString();
+            string sign = CreateMD5(timestr + key);
+            string kssj = Convert.ToInt64((startTime - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds).ToString();
+            string jzsj = Convert.ToInt64((endTime - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds).ToString();
+            int mpage = curpage;
+            int pgsize = 50;
+            string url = $"http://zjj.xtsuo.cn/api/evshopapi.php?i=1&time={timestr}&pwd={sign}&startime={kssj}&endtime={jzsj}&page={mpage}&psize={pgsize}";
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage res = httpClient.GetAsync(url).Result;
+            if (res.IsSuccessStatusCode)
+            {
+                string result = Regex.Unescape(res.Content.ReadAsStringAsync().Result);
+                return JsonConvert.DeserializeObject<GetThirdQueryOrdersResultDto>(result);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private DateTime ConvertStringToDateTime(string timeStamp)
+        {
+            long tsp = 0;
+            long.TryParse(timeStamp,out tsp);
+            long begtime = tsp * 10000000;
+            DateTime dt_1970 = new DateTime(1970, 1, 1, 0, 0, 0);
+            long tricks_1970 = dt_1970.Ticks;//1970年1月1日刻度
+            long time_tricks = tricks_1970 + begtime;//日志日期刻度
+            DateTime dt = new DateTime(time_tricks);//转化为DateTime
+            return dt;
+        }
+        private string CreateMD5(string input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var result = md5.ComputeHash(Encoding.Default.GetBytes(input));
+                var strResult = BitConverter.ToString(result).ToLower();
+                return strResult.Replace("-", "");
+            }
+        }
+
         public ActionResult PayNotifyUrl()
         {
+
             return null;//不用
         }
+
 
 
         #region  完结分账 这里不用 先不删（这里用单次分账 分账后 就结账）
